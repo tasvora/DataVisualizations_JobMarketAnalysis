eed1f77a79e2794e48994fa1ba12121f7eb72454
Sri Vidya <indupv@hotmail.com>
2019-12-05 13:11:20 -0500

    glassdoor data east coast upload

--- Histograms/static/app.js
+++ Histograms/static/app.js
@@ -1,9 +1,8 @@
 function buildChart() {
-
+  var traceGlassdoor, traceIndeed;
   var url = "/api/histogram/glassdoor";
   d3.json(url).then(function(response) {
-    console.log(response);
-    var trace1 = {
+    traceGlassdoor = {
     type: 'histogram',
     x: response.bins,
     y: response.jobs,
@@ -14,13 +13,29 @@ function buildChart() {
         }
     }
   };
+});
 
-  var data = [ trace1 ];
+url = "/api/histogram/indeed";
+  d3.json(url).then(function(response) {
+    console.log(response);
+    traceIndeed = {
+    type: 'histogram',
+    x: response.bins,
+    y: response.jobs,
+    marker: {
+        color: '#C8A2C8',
+        line: {
+            width: 2.5
+        }
+    }
+  };
+});
+
+var data = [ traceGlassdoor,traceIndeed ];
   var layout = {
       title: "Histogram Chart",
     };
   Plotly.newPlot('hist', data, layout, {responsive: true})
-});
 }
 
 buildChart() 
